3dc38d3f04cef9e060816ad3d2731201718c87d6
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-05-24 18:36:11 -0400

    js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.get('/', (req, res) => {
     // res.json(html);
     // res.sendFile(path.join('./public/index.html'));
     // res.sendFile('public/index.html', { root: public })
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(public, './index.html'));
 })
 server.use((req, res) => {
     res.status(404).json(`The end point: ${req.url} does not exist in our api`);
