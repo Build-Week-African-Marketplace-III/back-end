144a57d0302d1585f93782e77102d17aac0a59a1
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-05-24 17:46:44 -0400

    adding a public file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config(); // add this line as the first thing to run1
+const html = require('./public/index.html')
 const express = require('express');
 var cookieParser = require('cookie-parser')
 const server = express();
@@ -18,15 +19,7 @@ server.use('/api/', usersRouter);
 server.use('/api/', itemsRouter);
 
 server.get('/', (req, res) => {
-    res.send(`
-    Welcome sauti-africa's API !
-    Here are the base url and their endpoints:
-    https://sauti-africa.herokuapp.com/: to land in the app
-    https://sauti-africa.herokuapp.com/api/items: to retrieve items
-    https://sauti-africa.herokuapp.com/api/register: to register
-    https://sauti-africa.herokuapp.com/api/login: to login
-    https://sauti-africa.herokuapp.com/api/users: to retrieve users
-    `);
+    res.send(html);
 })
 server.use((req, res) => {
     res.status(404).json(`The end point: ${req.url} does not exist in our api`);
@@ -39,3 +32,11 @@ server.use((req, res) => {
 // }
 
 // I have to push this today
+
+     // Welcome sauti-africa's API !
+    //Here are the base url and their endpoints:
+    // https://sauti-africa.herokuapp.com/: to land in the app
+    // https://sauti-africa.herokuapp.com/api/items: to retrieve items
+    // https://sauti-africa.herokuapp.com/api/register: to register
+    // https://sauti-africa.herokuapp.com/api/login: to login
+    // https://sauti-africa.herokuapp.com/api/users: to retrieve 
