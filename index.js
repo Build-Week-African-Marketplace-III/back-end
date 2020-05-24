cb8d8e1a6f5d3fd3832492d855bfd215b0718251
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-05-24 18:52:46 -0400

    js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config(); // add this line as the first thing to run1
-const path = require('./public/index.html')
+// const path = require('./public/index.html')
 const express = require('express');
 var cookieParser = require('cookie-parser')
 const server = express();
@@ -19,9 +19,9 @@ server.use('/api/', usersRouter);
 server.use('/api/', itemsRouter);
 
 server.get('/', (req, res) => {
-    // res.json(html);
+    res.send(`Welcome to Sauti-africa `);
     // res.sendFile(path.join('./public/index.html'));
-    res.sendFile(path.join(__dirname,'./public/index.html'));
+    // res.sendFile(path.join(__dirname,'./public/index.html'));
 })
 server.use((req, res) => {
     res.status(404).json(`The end point: ${req.url} does not exist in our api`);
