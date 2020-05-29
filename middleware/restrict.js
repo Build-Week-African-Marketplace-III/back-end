2b60eb41ee13483d22bd04a814acfa07049d4fc0
thiernodiallo222 <thiernodiallo864@gmail.com>
2020-05-28 20:13:25 -0400

    just

--- middleware/restrict.js
+++ middleware/restrict.js
@@ -12,7 +12,7 @@ function restrict(role = "admin") {
             // this utilizes the `cookie-parser` middleware 
             // to pull the JWT out of the cookies that
 			//  were automatically sent by the client.
-			const token = req.headers.token
+			const token = req.headers.authorization;
  			if (!token) {
 				return res.status(401).json(authError)
 			}
